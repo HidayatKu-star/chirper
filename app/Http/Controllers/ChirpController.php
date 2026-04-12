55845b19d932309f39834041f932888d03a739e6
helminolimit <58017701+helminolimit@users.noreply.github.com>
2026-04-12 16:06:09 +0800

    Our first model
    Build your first Eloquent model to interact with the database elegantly. Learn how models represent database tables as PHP objects with powerful features.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chirp;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
     public function index()
-{
-        return view('home');
+    {
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(50)  // Limit to 50 most recent chirps
+            ->get();
+
+        return view('home', ['chirps' => $chirps]);
     }
 }

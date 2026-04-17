920cb57ba43ccd8d092852f3da5a22813b97ee77
helminolimit <58017701+helminolimit@users.noreply.github.com>
2026-04-17 10:08:13 +0800

    laravel boost
    installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Laravel\Boost\Boost;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Boost::registerAgent('customagent', CustomAgent::class);
     }
 }
